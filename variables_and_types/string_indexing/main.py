d80071248d6aefdcc0c765204670092003e9d6aa
BspearsTN <>
2026-04-19 04:10:10 +0000

    String Indexing

---
+++ variables_and_types/string_indexing/main.py
@@ -0,0 +1,17 @@
+grocery_item = "Grilled Chicken Salad"
+print(grocery_item[0])
+print(grocery_item[8])
+print(grocery_item[16])
+length_of_item = len(grocery_item)
+print(grocery_item[-1])
+print(grocery_item[-7])
+print(grocery_item[-15])
+first_char = (grocery_item [0])
+second_char = (grocery_item [8])
+third_char = (grocery_item [16])
+last_char1 = (grocery_item [-1])
+last_char2 = (grocery_item [-7])
+last_char3 = (grocery_item [-15])
+# Testing
+print("First character of each word:", first_char, second_char, third_char)
+print("Last character of each word:", last_char1, last_char2, last_char3)
